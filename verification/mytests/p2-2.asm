f68ab014b8c50b80182445b247073dd93e7b9be7
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2022-04-25 22:23:05 -0500

    Delete verification directory

--- verification/mytests/p2-2.asm
+++
@@ -1,9 +0,0 @@
-// Write your assembly program for Problem 2 (a) #2 here.
-
-lbi  r1, -5			// loop variable r1 with initial value -5
-.loop:
-beqz r1, .end		// exit the loop when r1 reaches 0
-addi r1, r1, 1		// increment r1 by 1 in each iteration
-j	 .loop
-.end:
-halt
